dad5ed7478c175dbdc7aa074e8093886fa16f9cd
sSpicey <martini@alunos.utfpr.edu.br>
2023-01-07 15:25:21 -0300

    Add routes for AccountController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AccountController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('balance', [AccountController::class, 'show']);
+Route::post('event', [AccountController::class, 'store']);
+Route::post('reset', [AccountController::class, 'reset']);
