9e04b06292e9d292869fdbcf70b07b5042d0ec89
sSpicey <martini@alunos.utfpr.edu.br>
2023-01-10 00:37:11 -0300

    Refactor account controller store function

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -31,89 +31,92 @@ class AccountController extends Controller
 
     public function store(Request $request)
     {
-        $amount = $request->input('amount');
-
         switch ($request->type) {
             case 'deposit':
-                $destination = $request->input('destination');
-                $account_exists = DB::table('accounts')->where('id', $destination)->exists();
-
-                if (!$account_exists) {
-                    $account = Account::create(['id' => $destination, 'balance' => $amount]);
-
-                    $response = ['destination' => ['id' => $destination, 'balance' => $amount]];
-
-                    return response($response, 201);
-                }
-                else {
-                    $account = Account::find($destination);
-
-                    $account->balance += $amount;
-                    $account->save();
-
-                    $response = ['destination' => ['id' => $destination, 'balance' => $account->balance]];
-                    return response($response, 201);
-                }
-
+                return $this->deposit($request);
                 break;
             case 'withdraw':
-                $origin = $request->input('origin');
-                $account_exists = DB::table('accounts')->where('id', $origin)->exists();
-
-                if (!$account_exists) {
-                    return response(0, 404);
-                }
-                else {
-                    $origin = $request->input('origin');
-                    $account = Account::find($origin);
-
-                    $account->balance -= $amount;
-                    $account->save();
-
-                    $response = ['origin' => ['id' => $origin, 'balance' => $account->balance]];
-                    return response($response, 201);
-                }
-                
+                return $this->withdraw($request);
                 break;
             case 'transfer':
-                $origin = $request->input('origin');
-                $destination = $request->input('destination');
-                $account_exists = DB::table('accounts')->where('id', $origin)->exists();
-
-                if (!$account_exists) {
-                    return response(0, 404);
-                }
-                else {
-                    $origin_acc = Account::find($origin);
-                    
-
-                    $dest_exists = DB::table('accounts')->where('id', $destination)->exists();
-
-                    if($dest_exists){
-                        $dest_acc = Account::find($destination);
-
-                        $origin_acc->balance -= $amount;
-                        $dest_acc->balance += $amount;
-
-                        $origin_acc->save();
-                        $dest_acc->save();
-                    }
-                    else {
-                        $dest_acc = Account::create(['id' => $destination, 'balance' => $amount]);
-
-                        $origin_acc->balance -= $amount;
-
-                        $origin_acc->save();
-                    }
-
-                    $response = ['origin' => ['id' => strval($origin_acc->id), 'balance' => $origin_acc->balance], 
-                                 'destination' => ['id' => strval($dest_acc->id), 'balance' => $dest_acc->balance]];
-
-                    return response($response, 201);
-                }
+                return $this->transfer($request);
                 break;
-        
         }
-        return response('OK', 200);
+    }
+
+    private function deposit(Request $request){
+        $amount = $request->input('amount');
+        $destination = $request->input('destination');
+        $account_exists = DB::table('accounts')->where('id', $destination)->exists();
+
+        if (!$account_exists) {
+            $account = Account::create(['id' => $destination, 'balance' => $amount]);
+
+            $response = ['destination' => ['id' => $destination, 'balance' => $amount]];
+
+            return response($response, 201);
+        }
+            $account = Account::find($destination);
+
+            $account->balance += $amount;
+            $account->save();
+
+            $response = ['destination' => ['id' => $destination, 'balance' => $account->balance]];
+            return response($response, 201);
+    }
+
+    private function withdraw(Request $request){
+        $amount = $request->input('amount');
+        $origin = $request->input('origin');
+        $account_exists = DB::table('accounts')->where('id', $origin)->exists();
+
+        if (!$account_exists) {
+            return response(0, 404);
+        }
+            $origin = $request->input('origin');
+            $account = Account::find($origin);
+
+            $account->balance -= $amount;
+            $account->save();
+
+            $response = ['origin' => ['id' => $origin, 'balance' => $account->balance]];
+            return response($response, 201);
+    }
+
+    private function transfer(Request $request){
+        $amount = $request->input('amount');
+        $origin = $request->input('origin');
+        $destination = $request->input('destination');
+        $account_exists = DB::table('accounts')->where('id', $origin)->exists();
+
+        if (!$account_exists) {
+            return response(0, 404);
+        }
+
+        $origin_acc = Account::find($origin);
+
+        $dest_exists = DB::table('accounts')->where('id', $destination)->exists();
+
+        if ($dest_exists) {
+            $dest_acc = Account::find($destination);
+
+            $origin_acc->balance -= $amount;
+            $dest_acc->balance += $amount;
+
+            $origin_acc->save();
+            $dest_acc->save();
+        }
+        else {
+            $dest_acc = Account::create(['id' => $destination, 'balance' => $amount]);
+
+            $origin_acc->balance -= $amount;
+            
+            $origin_acc->save();
+        }
+
+        $response = ['origin' => ['id' => strval($origin_acc->id), 'balance' => $origin_acc->balance], 
+                     'destination' => ['id' => strval($dest_acc->id), 'balance' => $dest_acc->balance]];
+
+        return response($response, 201);
     }
 }
